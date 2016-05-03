16f3d72de34afb5a20762c8d229e4ce50f7d1863
Matthew Cummings <zombymatthew@gmail.com>
2016-05-03 14:14:25 -0700

    Adding hiberate validation to form

--- src/com/zombymatthew/spring/web/controllers/OffersController.java
+++ src/com/zombymatthew/spring/web/controllers/OffersController.java
@@ -2,9 +2,13 @@ package com.zombymatthew.spring.web.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,9 +49,21 @@ public class OffersController
   }
 
   @RequestMapping(value="/docreate", method=RequestMethod.POST)
-  public String doCreate (Model model, Offer offer)
+  public String doCreate (Model model, @Valid Offer offer, BindingResult result)
   {
-    System.out.println (offer); 
+    if (result.hasErrors ())
+    {
+      List<ObjectError> errors = result.getAllErrors (); 
+      for (ObjectError error: errors)
+      {
+        System.out.println ("Form does not validate: "+ error.getDefaultMessage ());
+      }
+    }
+    else
+    {
+      System.out.println (offer); 
+    }
+
     return "offercreated";
   }
 
--- src/com/zombymatthew/spring/web/dao/Offer.java
+++ src/com/zombymatthew/spring/web/dao/Offer.java
@@ -1,10 +1,19 @@
 package com.zombymatthew.spring.web.dao;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class Offer
 {
   private int id;
+  
+  @Size(min=5, max=100, message="Name must be between 5 and 100 characters")
   private String name;
+  
+  @Pattern(regexp=".+\\@.+\\..+", message="Email not valid")
   private String email;
+
+  @Size(min=20, max=255, message="Text must be between 20 and 255 characters")
   private String text;
   
   public Offer ()
