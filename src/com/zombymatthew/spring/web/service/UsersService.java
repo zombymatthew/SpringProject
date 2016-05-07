e9bb6993b9f39a0074e850bbadd865bb506ffc4a
Matthew Cummings <zombymatthew@gmail.com>
2016-05-07 15:44:55 -0700

    Added Users database layers

---
+++ src/com/zombymatthew/spring/web/service/UsersService.java
@@ -0,0 +1,26 @@
+package com.zombymatthew.spring.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zombymatthew.spring.web.dao.User;
+import com.zombymatthew.spring.web.dao.UsersDAO;
+
+@Service
+public class UsersService
+{
+  private UsersDAO usersDao;
+  
+  @Autowired
+  public void setUsersDao (UsersDAO usersDao)
+  {
+    this.usersDao = usersDao;
+  }
+
+  
+  public boolean create (User user)
+  {
+    return usersDao.create (user);
+  }
+
+}
